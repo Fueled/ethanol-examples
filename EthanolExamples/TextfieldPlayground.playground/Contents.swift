6ab5088a128ccd4e9619aad978658a3a2b22006d
Bastien Falcou <bastien@fueled.com>
2015-08-12 11:47:22 -0400

    feat(TextfieldPlayground): added simple playground.

---
+++ EthanolExamples/TextfieldPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+
+import Cocoa
+//import Ethanol
+
+/*:
+# ETHTextField Playground
+Learn how to use ETHTextField in your project and enjoy its numerous benefits such as:
+* String validation
+* String formatting
+* Convenience properties
+* And many others.
+*/
+
+//var ethTextfield: ETHTextField! = ETHTextField()
