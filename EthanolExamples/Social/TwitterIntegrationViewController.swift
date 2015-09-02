95d3070d6b3750606827a23104379e546ef9d672
sambhav-fueled <sambhav@fueled.com>
2015-09-02 17:04:53 +0530

    separated twitter and facebook viewControllers

--- EthanolExamples/Social/TwitterIntegrationViewController.swift
+++ EthanolExamples/Social/TwitterIntegrationViewController.swift
@@ -9,18 +9,6 @@
 import Foundation
 import EthanolSocial
 
-class FacebookIntegrationViewController: UIViewController {
-
-	@IBAction func loginTapped(sender: AnyObject) {
-    SocialManager.serviceForType(serviceType: .Facebook).loginSilentlyIfPossible(permissions: [FacebookSocialServicePermission.PublicProfile, FacebookSocialServicePermission.Email], success: { (success) in
-      print("Success : \(success)")
-      print("user is : \(SocialManager.serviceForType(serviceType: .Facebook).currentUser?.socialServiceDescription)")
-      }, failure: { (error) in
-        print("error: \(error)")
-    })
-  }
-}
-
 class TwitterIntegrationViewController: UIViewController {
 
 	@IBAction func loginTapped(sender: AnyObject) {
