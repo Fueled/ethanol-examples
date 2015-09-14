6e87d27ad5bad4cf20f25e7dc84126a829248a1a
harish-fueled <harish@fueled.com>
2015-09-14 18:21:44 +0530

    feat(ResourceFetcher) : custom fetcher

--- EthanolExamples/Utilities/FourSquareResourceFetcher.swift
+++ EthanolExamples/Utilities/FourSquareResourceFetcher.swift
@@ -1,5 +1,5 @@
 //
-//  FourSquareResourceFetcher.swift
+//  CustomResourceFetcher.swift
 //  EthanolExamples
 //
 //  Created by hhs-fueled on 14/09/15.
@@ -9,8 +9,26 @@
 import UIKit
 import EthanolUtilities
 
-class FourSquareResourceFetcher: ResourceFetcher {
+class CustomResourceFetcher: ResourceFetcher {
 	override func fetchPage(pageNumber: Int, pageLimit: Int, completion: ExternalAPICompletionHandler?) {
+		let delayTime = 2.0
+		delay(delayTime) { () -> () in
+			var randomArray: [String] = []
+			for index in 0..<pageLimit {
+				randomArray.append("Page - \(pageNumber) - \(index)")
+			}
 
+			let error = NSError(domain: "APIERROR", code: 400, userInfo: [NSLocalizedDescriptionKey : "NOTHING ERROR"])
+
+			let successChance = 90 // percent success chance
+			let success = (random() % 100 <= successChance)
+
+			if success {
+				completion?(){ return randomArray }
+			} else {
+				completion?(){ throw error }
+			}
+		}
+		
 	}
 }
--- EthanolExamples/Utilities/ResourcesExampleTableViewController.swift
+++ EthanolExamples/Utilities/ResourcesExampleTableViewController.swift
@@ -13,22 +13,116 @@ let resourceFetcherCellIdentifier = "ResourceFetcherCellIdentifier"
 
 class ResourcesExampleTableViewController: UITableViewController {
 
-	let fourSquareFetcher = FourSquareResourceFetcher()
+	let customFetcher = CustomResourceFetcher()
+	var dataSource: [AnyObject] = []
+	var loadMoreActivity: UIActivityIndicatorView?
+	var isLoading = false
+	var needsToLoadMoreData = true
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		customFetcher.startFetchingProducts { (inner) -> Void in
+			do {
+				let result = try inner()
+				self.dataSource = result.objects
+				self.tableView.reloadData()
+			} catch {
+				print("error occurred.")
+			}
+		}
 	}
 
+	func addLoadMore(){
+		if self.tableView.numberOfRowsInSection(0) == 0 {
+			return;
+		}
+		guard let _ = self.loadMoreActivity else {
+			self.loadMoreActivity = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+			let screenWidth = UIScreen.mainScreen().bounds.size.width
+			self.loadMoreActivity!.frame = CGRectMake(0.0, 0.0, screenWidth, 44.0);
+			self.loadMoreActivity?.startAnimating()
+			self.tableView.tableFooterView = self.loadMoreActivity
+			return
+		}
+
+		self.loadMoreActivity?.startAnimating()
+		self.tableView.tableFooterView = self.loadMoreActivity
+
+	}
+
+	func removeLoadMore() {
+		loadMoreActivity?.stopAnimating()
+		self.tableView.tableFooterView = nil;
+	}
+
+
+	func fetchNextPage() {
+		if isLoading {
+			return
+		}
+		isLoading = true
+		customFetcher.fetchNextPage { (inner) -> Void in
+			do {
+				let result = try inner()
+				self.dataSource += result.objects
+				self.appendCellsForMoreObjects(result.objects.count)
+//				self.tableView.reloadData()
+				self.needsToLoadMoreData = result.hasMoreDataToLoad
+				self.isLoading = false
+			} catch {
+				print("error occurred.")
+			}
+		}
+	}
+
+	func appendCellsForMoreObjects(objectsCount:Int) {
+
+		let previousCellCount = self.tableView.numberOfRowsInSection(0)
+
+		let nextCellCount = previousCellCount + objectsCount;
+		if nextCellCount > previousCellCount {
+			var indexPaths: [NSIndexPath] = []
+			for counter in previousCellCount..<nextCellCount {
+				let indexPath = NSIndexPath(forRow: counter, inSection: 0)
+				indexPaths.append(indexPath)
+			}
+			tableView.beginUpdates()
+			tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.None)
+			tableView.endUpdates()
+		}
+	}
 
 	// MARK: - Table view data source
 
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 10
+		return dataSource.count
 	}
 
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier(resourceFetcherCellIdentifier, forIndexPath: indexPath)
-		cell.textLabel?.text = "test"
+		let fetchedObject = dataSource[indexPath.row] as! String
+		cell.textLabel?.text = fetchedObject
 		return cell
+
 	}
+
+	// MARK: UIScrollViewDelegate Methods
+	override func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+		if shouldShowLoadMoreFor(scrollView) && scrollView.contentOffset.y > 0 {
+			fetchNextPage()
+		}
+	}
+
+	func shouldShowLoadMoreFor(scrollView:UIScrollView) -> Bool{
+		let currentOffset = scrollView.contentOffset
+		let scrollViewBounds = scrollView.bounds
+		let scrollViewContentSize = scrollView.contentSize
+		let scrollViewInsets = scrollView.contentInset
+
+		let bottomScrolledYPosition = currentOffset.y + scrollViewBounds.size.height + scrollViewInsets.bottom
+		let scrollViewContentHeight = scrollViewContentSize.height
+		let reloadDistance: CGFloat = 0.0
+		return ((bottomScrolledYPosition - scrollViewContentHeight + reloadDistance - 44.0 + 100.0) > 0 && self.needsToLoadMoreData);
+	}
+	
 }
