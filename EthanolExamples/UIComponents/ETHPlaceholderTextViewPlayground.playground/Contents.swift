9a38b22d43f3255863e4ac53da4c02cf758d37bc
Bastien Falcou <bastien@fueled.com>
2016-03-29 12:49:50 -0400

    feat(ETHPlaceholderTextViewPlayground): example placeholder text view

---
+++ EthanolExamples/UIComponents/ETHPlaceholderTextViewPlayground.playground/Contents.swift
@@ -0,0 +1,36 @@
+
+import EthanolUIComponents
+/*:
+Finally a Text View that supports a placeholder text!
+-----
+__Tip:__ EthanolUIComponents supports IBDesignable and IBInspectable for its properties.
+*/
+
+let ethPlaceholderTextView = ETHPlaceholderTextView(frame: CGRectMake(0.0, 0.0, 150.0, 50.0))
+
+/*:
+## Regular placeholder text
+Specify some text to your placeholder. Text color will be default iOS placeholder color.
+*/
+ethPlaceholderTextView.placeholder = "I am a placeholder"
+
+// Changing text view font also udpates placeholder font 
+ethPlaceholderTextView.font = UIFont.systemFontOfSize(35.0)
+
+/*:
+## What if we type in some text?
+*/
+ethPlaceholderTextView.font = UIFont.systemFontOfSize(12.0)
+ethPlaceholderTextView.text = "I am the actual text"
+
+/*:
+## Attributed Text Placeholder
+Specify custom attributed text as placeholder.
+*/
+let placeholderText = "I am a custom placeholder"
+let attributedString = NSMutableAttributedString(string: placeholderText)
+attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor.lightGrayColor(), range: NSMakeRange(0, placeholderText.characters.count))
+attributedString.addAttribute(NSFontAttributeName, value: (ethPlaceholderTextView.font?.fontWithSize(10.0))!, range: NSMakeRange(0, placeholderText.characters.count))
+
+ethPlaceholderTextView.text = ""
+ethPlaceholderTextView.attributedPlaceholder = attributedString
